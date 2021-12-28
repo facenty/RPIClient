b5ffeed87e7c9735b6fd4ef6d8004425a4051d28
Oskar Chodowicz <ochodowicz@vewd.com>
2021-12-28 18:27:59 +0100

    Something is workign

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,30 @@
 #include <string>
 #include <thread>
 #include <iomanip>
+#include <chrono>
+#include <sstream>
+#include <functional>
+#include <experimental/optional>
 #include <experimental/string_view>
 
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
 #include <boost/asio/serial_port.hpp>
 #include <boost/asio/io_service.hpp>
 #include <boost/asio/read.hpp>
 #include <boost/asio/write.hpp>
 #include <boost/log/trivial.hpp>
+#include <boost/asio/high_resolution_timer.hpp>
 
 namespace
 {
 
-  constexpr const char kATCommand[] = "AT\n";
+  constexpr const char kATCommand[] = "AT\r\n";
   constexpr const char kOKReply[] = "OK";
   constexpr const char kErrorReply[] = "ERROR";
   constexpr const char kSerialName[] = "/dev/serial0";
+  using namespace std::chrono_literals;
+  // using namespace std::placeholders;
 
   auto initialize()
   {
@@ -32,16 +41,136 @@ namespace
     }
   }
 
+  struct ValueOrError {
+    bool success() {
+      return bool(result);
+    }
+    std::experimental::optional<std::string> error;
+    std::experimental::optional<std::string> result;
+  };
+
+  class Sim800Gprs {
+    public:
+      using ResultCallback = std::function<void(ValueOrError)>;
+      using Timeout = boost::asio::high_resolution_timer;
+      Sim800Gprs(boost::asio::serial_port& serialPort) : serialPort_(serialPort), ioService_(serialPort_.get_io_service()), timeout_(ioService_) {
+        // tmpBuffer_.reserve(1024);
+        // result = {'A', 'T', '\n', 'O', 'K'};
+        // std::cout << ContainsOk() << std::endl;
+
+      }
+      void ExecuteAtCommand(const std::vector<char>& atCommand, ResultCallback cb, std::chrono::milliseconds timeout = 2s) {
+        cb_ = std::move(cb);
+        timeout_.expires_from_now(timeout);
+        serialPort_.write_some(boost::asio::buffer(atCommand));
+        serialPort_.async_read_some(boost::asio::buffer(tmpBuffer_),
+            boost::bind(&Sim800Gprs::ReadSomeUntilResultOrTimeout, this, boost::asio::placeholders::error,
+            boost::asio::placeholders::bytes_transferred));
+        timeout_.async_wait(boost::bind(&Sim800Gprs::OnTimeout, this, boost::asio::placeholders::error));
+        timeouted_ = false;
+      }
+
+    private:
+      bool ContainsError() {
+        return Contains(kErrorReply);
+      }
+
+      bool ContainsOk() {
+        return Contains(kOKReply);
+      }
+
+      bool Contains(const std::string& searchWord) {
+        auto it = std::search(result.begin(), result.end(), searchWord.begin(), searchWord.end());
+        return it != result.end();
+      }
+
+      void ReadSomeUntilResultOrTimeout(const boost::system::error_code &error, std::size_t readBytes) {
+        if (readBytes) {
+          std::cout << readBytes << std::endl;
+        }
+        if (timeouted_ == true) {
+          return;
+        }
+        if (error) {
+          PostCallbackWithError(error.message());
+          return;
+        }
+        result.insert(result.end(), tmpBuffer_.begin(), tmpBuffer_.begin() + readBytes );
+        std::cout << "##############" << std::endl;
+        for (auto it : result) {
+          std::cout << "[" << std::hex << static_cast<int>(it) << "] ";
+        }
+        std::cout << std::endl;
+        std::cout << "OK: " << ContainsOk() << " ERROR: " << ContainsError() << std:: endl;
+        if (!ContainsError() && !ContainsOk()) {
+        std::cout << "Here reading" << std::endl;
+          serialPort_.async_read_some(boost::asio::buffer(tmpBuffer_),
+            boost::bind(&Sim800Gprs::ReadSomeUntilResultOrTimeout, this, boost::asio::placeholders::error,
+            boost::asio::placeholders::bytes_transferred));
+
+          return;
+        }
+        if (ContainsError()) {
+          PostCallbackWithError(std::string(result.begin(), result.end()));
+          return; 
+        }
+        std::cout << "Here" << std::endl;
+        PostCallbackWithResult(std::string(result.begin(), result.end()));
+      }
+
+      void OnTimeout(const boost::system::error_code& error)
+      {
+        if (!error) {
+          timeouted_ = true;
+          PostCallbackWithError("Request timeouted");
+        }
+      }
+
+      void PostCallbackWithError(const std::string& error) {
+        ValueOrError vor;
+        vor.error = error;
+        PostCallbackWithValueOrError(std::move(vor));
+      }
+
+      void PostCallbackWithResult(const std::string& result) {
+        ValueOrError vor;
+        vor.result = result;
+        PostCallbackWithValueOrError(std::move(vor));
+      }
+
+      void PostCallbackWithValueOrError(ValueOrError vor) {
+        timeout_.cancel();
+        ioService_.post(std::bind(std::move(cb_), std::move(vor)));
+      }
+
+    private:
+      boost::asio::serial_port& serialPort_;
+      boost::asio::io_service& ioService_;
+      std::vector<char> tmpBuffer_ = std::vector<char>(1024);
+      std::vector<char> result;
+      Timeout timeout_;
+      bool timeouted_;
+      ResultCallback cb_;
+  };
+
 } // namespace
 
 int main(int argc, char *argv[])
 {
   initialize();
 
-  std::vector<std::string> args;
-  args.reserve(argc);
-  for (auto i = 0u; i < static_cast<uint>(argc); ++i)
-    args.push_back(argv[i]);
+  // std::cout << "Boost version: " 
+  //         << BOOST_VERSION / 100000
+  //         << "."
+  //         << BOOST_VERSION / 100 % 1000
+  //         << "."
+  //         << BOOST_VERSION % 100 
+  //         << std::endl;
+
+  // std::vector<std::string> args;
+  // args.reserve(argc);
+  // for (auto i = 0u; i < static_cast<uint>(argc); ++i)
+  //   args.push_back(argv[i]);
 
   boost::system::error_code ec;
   boost::asio::io_service io_service;
@@ -54,30 +183,17 @@ int main(int argc, char *argv[])
   }
   serial_port.set_option(boost::asio::serial_port::baud_rate(115200));
 
-  std::vector<char> buffer(1024);
-  serial_port.write_some(boost::asio::buffer(kATCommand));
-  std::this_thread::sleep_for(std::chrono::milliseconds(500));
-  serial_port.async_read_some(boost::asio::buffer(buffer),
-                              [&](const boost::system::error_code &error, std::size_t read_bytes) {
-                                if (!error)
-                                {
-                                  buffer.resize(read_bytes);
-                                  std::experimental::string_view sv(buffer.data(), read_bytes);
-                                  BOOST_LOG_TRIVIAL(info) << "Read " << sv.size() << " characters: ";
-                                  BOOST_LOG_TRIVIAL(info) << sv;
 
-                                  //               BOOST_LOG_TRIVIAL(info) << "Read " << read_bytes << " characters: ";
-                                  // for (auto i = 0u; i < read_bytes; ++i) {
-                                  //   BOOST_LOG_TRIVIAL(info) << " [" << i << "] = [" << std::hex << static_cast<int>(buffer[i]) << "]";
-                                  // }
-                                }
-
-                                else
-                                {
-                                  BOOST_LOG_TRIVIAL(info) << "Error: " << error.message();
-                                }
-                              });
-
+  Sim800Gprs Sim800Gprs(serial_port);
+  Sim800Gprs.ExecuteAtCommand({kATCommand, kATCommand + strlen(kATCommand)}, [](ValueOrError voe) {
+    std::cout << "Success: " << voe.success() ? "YES" : "NO";
+    if (voe.success()) {
+      std::cout << *voe.result << std::endl;
+      return;
+    }
+    std::cout << *voe.error << std::endl;
+  });
+  
   io_service.run();
   return EXIT_SUCCESS;
 }
